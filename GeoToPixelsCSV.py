175bdc39619b2ada1b0b36e71526edf847f53640
Khanatoshs <leo.hs334@gmail.com>
2022-10-27 12:08:30 +0900

    Finished correcting script, now works and prints status

--- GeoToPixelsCSV.py
+++ GeoToPixelsCSV.py
@@ -1,3 +1,4 @@
+from genericpath import isfile
 import os
 import traceback
 from typing import Iterable
@@ -9,6 +10,15 @@ import fiona
 import configparser
 
 
+def makeConf():
+    strConf = '[GEOTOPIXEL]+\n'
+    strConf += 'tiff=\n'
+    strConf += 'shape=\n'
+    strConf += 'outfolder=\n'
+    strConf += 'outfilename=\n'
+    strConf += 'delimiter=,\n'
+    with open('config.ini','w') as confF:
+        confF.write(strConf)
 
 def read_config(filename) -> dict:
     conf = configparser.ConfigParser()
@@ -28,20 +38,6 @@ def read_config(filename) -> dict:
     }
     return ret_dict
 
-# def writeHeader(file:TextIOWrapper,delimiter:str,columns:Iterable):
-#     colStr = ''
-#     for col in columns:
-#         colStr += col + delimiter
-#     colStr = colStr[-1] + '\n'
-#     file.write(colStr)
-
-# def writeCSVLine(file: TextIOWrapper,delimiter:str,data:Iterable):
-#     dataStr = ''
-#     for dat in data:
-#         dataStr += str(dat) + delimiter
-#     dataStr = dataStr[-1] + '\n'
-#     file.write(dataStr)
-
 class CustomCSV:
     def __init__(self,filename:str,mode:str) -> None:
         self.csvFile = open(filename,mode)
@@ -53,60 +49,66 @@ class CustomCSV:
         if exc_type is not None:
             traceback.print_exception(exc_type, exc_value, tb)
         self.csvFile.close()
-#TODO: check why it doesn't print data
+
     def writeHeader(self,delimiter:str,columns:Iterable) -> None:
         colStr = ''
         for col in columns:
             colStr += col + delimiter
-        colStr = colStr[-1] + '\n'
+        colStr = colStr[:-1] + '\n'
         self.csvFile.write(colStr)
     
     def writeLineCSV(self,delimiter:str,data:Iterable) -> None:
         dataStr = ''
         for dat in data:
             dataStr += str(dat) + delimiter
-        dataStr = dataStr[-1] + '\n'
+        dataStr = dataStr[:-1] + '\n'
         self.csvFile.write(dataStr)
 
+def main():
+    if not os.path.exists('config.ini'):
+        print('THE FILE config.ini WAS CREATED, PLEASE INPUT INITIAL DATA IN FILE')
+        makeConf()
 
+    conf = read_config('config.ini')
 
+    if not os.path.isdir(conf.get('outfolder')):
+        os.mkdir(conf.get('outfolder'))
 
-conf = read_config('config.ini')
-
-if not os.path.isdir(conf.get('outfolder')):
-    os.mkdir(conf.get('outfolder'))
-
-outfile = os.path.join(conf.get('outfolder'), conf.get('outfilename') +'.csv')
-delim = conf.get('delimiter')
-shapes = None
-shp_geometry = None
-geoType = ''
-with fiona.open(conf.get('shape'),'r') as src_shp:
-    print(src_shp.schema)
-    # geoType = str(src_shp.schema['geometry'])
-    if geoType == 'Point': 
-        shapes = list(map(lambda i: {'id': i['id'],'coordinates': i['geometry']['coordinates']},src_shp))
-    else:
-        shapes = list(map(lambda i: {'id': i['properties']['MERGE_SRC'] + '_' + str(i['properties']['id']),'coordinates': i['geometry']['coordinates'][0] if len(i['geometry']['coordinates']) <=1 else i['geometry']['coordinates']},src_shp))
-
-
-with rasterio.open(conf.get('tiff')) as src_tiff:
-    with CustomCSV(outfile,'w') as outf: 
-        if geoType == 'Point':
-            outf.writeHeader(delim,('id','geoX','geoY','pX','pY'))
-            # writeHeader(outf,delim,('id','coordx','coordy','px','py'))
+    outfile = os.path.join(conf.get('outfolder'), conf.get('outfilename') +'.csv')
+    delim = conf.get('delimiter')
+    shapes = None
+    geoType = ''
+    print('--- OPENING SHAPE FILE: ' + conf.get('shape') + ' ---')
+    with fiona.open(conf.get('shape'),'r') as src_shp:
+        # print(src_shp.schema['geometry'])
+        geoType = str(src_shp.schema['geometry'])
+        if geoType == 'Point': 
+            shapes = list(map(lambda i: {'id': i['id'],'coordinates': i['geometry']['coordinates']},src_shp))
         else:
-            outf.writeHeader(delim,('polyId','pointId','geoX','geoY','pX','pY'))
-            # writeHeader(outf,delim,('polyId','pointId','coordy','geoCoords','pixCoords'))
-        for shp in shapes:
-            if geoType != 'Point':
-                for i,cor in enumerate(shp['coordinates']):
-                    px,py = src_tiff.index(*cor)
-                    outf.writeLineCSV(delim,(str(shp['id']),str(i+1),str(cor[0]),str(cor[1]),str(px),str(py)))
-                    # outf.write(str(shp['id']) + ';'  + str(i+1) + ';' + str(cor) + ';' + str((px,py)) + '\n')
-            else:
-                px,py = src_tiff.index(*shp['coordinates'])
-                outf.writeLineCSV(delim,str(shp['id']),str(shp['coordinates'][0]),str(shp['coordinates'][1]),str(px),str(py))
-                # outf.write(str(shp['id']) + ',' + str(shp['coordinates'][0]) + ',' + str(shp['coordinates'][1]) + ',' + str(px) + ',' + str(py) + '\n')
-            
+            shapes = list(map(lambda i: {'id': i['properties']['MERGE_SRC'] + '_' + str(i['properties']['id']),'coordinates': i['geometry']['coordinates'][0] if len(i['geometry']['coordinates']) <=1 else i['geometry']['coordinates']},src_shp))
 
+    print('--- FOUND ' + str(len(shapes)) + ' SHAPES OF TYPE: ' + str(geoType) + ' ---')
+    print('--- OPENING TIFF FILE: ' + conf.get('tiff') + ' ---')
+    with rasterio.open(conf.get('tiff')) as src_tiff:
+        print('--- WRITING TO FILE: ' + outfile  + ' ---')
+        with CustomCSV(outfile,'w') as outf: 
+            if geoType == 'Point':
+                outf.writeHeader(delim,('id','geoX','geoY','pX','pY'))
+                # writeHeader(outf,delim,('id','coordx','coordy','px','py'))
+            else:
+                outf.writeHeader(delim,('polyId','pointId','geoX','geoY','pX','pY'))
+                # writeHeader(outf,delim,('polyId','pointId','coordy','geoCoords','pixCoords'))
+            for shp in shapes:
+                if geoType != 'Point':
+                    for i,cor in enumerate(shp['coordinates']):
+                        px,py = src_tiff.index(*cor)
+                        outf.writeLineCSV(delim,(str(shp['id']),str(i+1),str(cor[0]),str(cor[1]),str(px),str(py)))
+                        # outf.write(str(shp['id']) + ';'  + str(i+1) + ';' + str(cor) + ';' + str((px,py)) + '\n')
+                else:
+                    px,py = src_tiff.index(*shp['coordinates'])
+                    outf.writeLineCSV(delim,str(shp['id']),str(shp['coordinates'][0]),str(shp['coordinates'][1]),str(px),str(py))
+                    # outf.write(str(shp['id']) + ',' + str(shp['coordinates'][0]) + ',' + str(shp['coordinates'][1]) + ',' + str(px) + ',' + str(py) + '\n')
+        print('--- FINISHED WRITING FILE ---')
+
+if __name__ == '__main__':
+    main()
