f3740afbbbcb3542c198c5109d9fba41da1e67a7
Khanatoshs <leo.hs334@gmail.com>
2023-03-29 22:08:48 +0900

    Finished correcting YoloToGPS now functional

--- YOLOToGPS.py
+++ YOLOToGPS.py
@@ -1,7 +1,9 @@
+import os
 from decimal import Decimal
 import rasterio
 import configparser
 import fiona
+import pprint
 
 def read_config(filename) -> dict:
     conf = configparser.ConfigParser()
@@ -20,18 +22,20 @@ def read_config(filename) -> dict:
     return ret_dict
 
 def yolo_to_pixel(yolcoord:dict,imwidth,imheight):
-# cat x y width height
-    px = yolcoord['x'] * imwidth
-    py = yolcoord['y'] * imheight
+# cat xc yc width height
+    pxc = yolcoord['x'] * imwidth
+    pyc = yolcoord['y'] * imheight
     pw = yolcoord['width'] * imwidth
     ph = yolcoord['height'] * imheight
-    pxc = px + (pw//2)
-    pyc = py + (ph//2)
+    pxf = pxc + (pw//2)
+    pyf = pyc + (ph//2)
+    px0 = pxc - (pw//2)
+    py0 = pyc - (ph//2)
     return {
-        'x': px,
-        'y':py,
-        'xf': px + pw,
-        'yf': py + ph,
+        'x': px0,
+        'y':py0,
+        'xf': pxf,
+        'yf': pyf,
         'xc': pxc,
         'yc': pyc,
         'width': pw,
@@ -56,41 +60,37 @@ def read_yolo_file(filename:str):
     
     yolList = (list(map(lambda i: split_yolo_line(i),lines)))
     return yolList
+def main():
+    conf = read_config('config.ini')
+    yoloList = read_yolo_file(conf['yoloFile'])
+    tiffh = None
+    tiffw = None
+    crs = None
+    with rasterio.open(conf['tiff']) as rastiff:
+        crs = str(rastiff.crs)
+        tiffh = rastiff.shape[0]
+        tiffw = rastiff.shape[1]
+        pixlist = list(map(lambda i: yolo_to_pixel(i, tiffw, tiffh), yoloList))
+        gpslist = list(map(lambda i:{'lat0':rastiff.xy(int(i['y']),int(i['x']))[0],'lon0':rastiff.xy(int(i['y']),int(i['x']))[1],'latf':rastiff.xy(int(i['yf']),int(i['xf']),)[0],'lonf':rastiff.xy(int(i['yf']),int(i['xf']))[1],'latc':rastiff.xy(int(i['yc']),int(i['xc']))[0],'lonc':rastiff.xy(int(i['yc']),int(i['xc']))[1],'cat':i['cat']},pixlist))
+        
+    schema = {
+        'geometry':'Point',
+        'properties':[('Category','str')]
+    }
 
-conf = read_config('config.ini')
-yoloList = read_yolo_file(conf['yoloFile'])
-tiffh = None
-tiffw = None
-crs = None
-with rasterio.open(conf['tiff']) as rastiff:
-    crs = str(rastiff.crs)
-    tiffh = rastiff.shape[0]
-    tiffw = rastiff.shape[1]
-    pixlist = list(map(lambda i: yolo_to_pixel(i, tiffw, tiffh), yoloList))
-    gpslist = list(map(lambda i:{'x0':rastiff.xy(int(i['x']),int(i['y']))[0],'y0':rastiff.xy(int(i['x']),int(i['y']))[1],'xf':rastiff.xy(int(i['xf']),int(i['yf']))[0],'yf':rastiff.xy(int(i['xf']),int(i['yf']))[1],'xc':rastiff.xy(int(i['xc']),int(i['yc']))[0],'yc':rastiff.xy(int(i['xc']),int(i['yc']))[1],'cat':i['cat']},pixlist))
+    outfile = os.path.join(conf.get('outfolder'), conf.get('outfilename') +'.shp')
 
-schema = {
-    'geometry':'Point',
-    'properties':[('Name','str')]
-}
+    with fiona.open(outfile, mode='w',driver='ESRI Shapefile',schema=schema,crs=crs) as newshp:
+        for gps in gpslist:
+            row = {
+                'geometry':{
+                    'type':'Point',
+                    'coordinates':(gps['latc'],gps['lonc'])},
+                'properties':{'Category': gps['cat']}
+            }
+            newshp.write(row)
 
-with fiona.open('results.shp', mode='w',driver='ESRI shapefile',schema=schema,crs=crs) as newshp:
-    for gps in gpslist:
-        row = {
-            'geometry':{
-                'type':'Point',
-                'coordinates':(gps['xc'],gps['yc'])},
-            'properties':{'Name': gps['cat']}
-        }
-
-
-# with open('results.csv','r') as res:
-    
-#     for gps in gpslist:
-#         res.write(str(gps['x0']) + ',' + str(gps['y0']) + ',' + str(gps['xf']) + ',' + str(gps['yf']))
-# print(yoloList[0])
-# print(gpslist[0])
-# print(pixlist[0])
-
+if __name__ == '__main__':
+    main()
 
 
--- cutTiff.py
+++
@@ -1,5 +0,0 @@
-import cv2
-import sys
-argv = sys.argv
-if len(argv)> 2:
-    cv2.imread(argv[2])
