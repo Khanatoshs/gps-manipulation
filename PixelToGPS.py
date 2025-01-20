7a968a88a4f420be2de0963d39d358549dd6feee
Khanatoshs <leo.hs334@gmail.com>
2025-01-20 16:39:49 +0900

    Finished the script to convert pixel to gps

--- PixelToGPS.py
+++ PixelToGPS.py
@@ -1,8 +1,7 @@
 import configparser
 import rasterio
-import cv2
 import fiona
-
+import os
 
 def read_config(filename) -> dict:
     conf = configparser.ConfigParser()
@@ -14,7 +13,7 @@ def read_config(filename) -> dict:
     outfilename = conf[section].get('outfilename')
     ret_dict = {
         'tiff':tiff,
-        'pixelFile':yoloFile,
+        'pixelfile':pixelfile,
         'outfolder':outfolder,
         'outfilename': outfilename
     }
@@ -35,11 +34,43 @@ def readPixels(pixelFile):
     pixelList = list(map(lambda i: splitLine(i),lines))
     return pixelList
 
-def transformPix():
-    return
+def transformPix(rasterpath,pixList:list):
+    geolist = []
+    crs = None
+    with rasterio.open(rasterpath) as raster:
+        crs = str(raster.crs)
+        for pix in pixList:
+            geo = raster.xy(pix['y'],pix['x'])
+            geolist.append({
+                'coord':geo['coords'],
+                'cat':pix['val']
+                })
+    return crs,geolist
+    
+
+def writeShapefile(geolist,outfilepath,crs):
+    schema = {
+        'geometry':'Point',
+        'properties':[('Category','Double')]
+    }
+
+    with fiona.open(outfilepath, mode='w',driver='ESRI Shapefile',schema=schema,crs=crs) as newshp:
+        for gps in geolist:
+            row = {
+                'geometry':{
+                    'type':'Point',
+                    'coordinates':gps},
+                'properties':{'Category': gps['cat']}
+            }
+            newshp.write(row)
 
 def main():
-    return
+    conf = read_config('config.ini')
+    pixlist = readPixels(conf['pixelfile'])
+    crs,geolist = transformPix(conf['tiff'],pixlist)
+    outfilepath = os.path.join(conf['outfolder'],conf['outfile'])
+    writeShapefile(geolist,outfilepath,crs)
+
 
 if __name__ == '__main__':
     main()
